7b502d57b29f5c9afc160d7d162040ddcba715c3
nabendu82 <nabendu.biswas@gmail.com>
2020-02-01 20:44:51 +0530

    Google Ads check

--- src/html.js
+++ src/html.js
@@ -5,7 +5,20 @@ export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
       <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
         {props.headComponents}
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <script>
+          (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-8060048775507010",
+            enable_page_level_ads: true
+          });
+        </script>
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
