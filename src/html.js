e23e2e1763ebb87054f0b0bb4bc3d8ee6b50e1ff
nabendu82 <nabendu.biswas@gmail.com>
2020-02-03 20:31:15 +0530

    media.net ads

--- src/html.js
+++ src/html.js
@@ -12,9 +12,13 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
-        <script type="text/javascript"> var infolinks_pid = 3233958; var infolinks_wsid = 0; </script>
-        <script type="text/javascript" src="//resources.infolinks.com/js/infolinks_main.js"></script>
-      </head>
+        <script type="text/javascript">
+          window._mNHandle = window._mNHandle || {};
+          window._mNHandle.queue = window._mNHandle.queue || [];
+          medianet_versionId = "3121199";
+        </script>
+        <script src="//contextual.media.net/dmedianet.js?cid=8CUVUWCU4" async="async"></script>
+        </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
         <noscript key="noscript" id="gatsby-noscript">
@@ -26,6 +30,16 @@ export default function HTML(props) {
           dangerouslySetInnerHTML={{ __html: props.body }}
         />
         {props.postBodyComponents}
+        <div id="322374539">
+          <script type="text/javascript">
+              try {
+                  window._mNHandle.queue.push(function (){
+                      window._mNDetails.loadTag("322374539", "160x600", "322374539");
+                  })
+              }
+              catch (error) {}
+          </script>
+        </div>
       </body>
     </html>
   )
