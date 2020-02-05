6be359814671295dc8abe975302c3d27b5c49904
nabendu82 <nabendu.biswas@gmail.com>
2020-02-05 19:16:29 +0530

    Changes again

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,16 +10,12 @@ const safePrefix = require("./src/utils/safePrefix").default;
 exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
 
     setHeadComponents([
-        <script
-            dangerouslySetInnerHTML={{
-                __html=`
-                window._mNHandle = window._mNHandle || {};
-                window._mNHandle.queue = window._mNHandle.queue || [];
-                medianet_versionId = "3121199";
-                `
-            }}
-        />,
-        <script src="//contextual.media.net/dmedianet.js?cid=8CUVUWCU4" async="async" />
+        <script type="text/javascript">
+            window._mNHandle = window._mNHandle || {};
+            window._mNHandle.queue = window._mNHandle.queue || [];
+            medianet_versionId = "3121199";
+        </script>,
+        <script src="//contextual.media.net/dmedianet.js?cid=8CUVUWCU4" async="async"></script>
     ]);
 
     setPostBodyComponents([
@@ -27,19 +23,15 @@ exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
             <script src={safePrefix('assets/js/plugins.js')}/>
             <script src={safePrefix('assets/js/main.js')}/>
             <div id="322374539">
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html=`
-                        try {
-                            window._mNHandle.queue.push(function (){
-                                window._mNDetails.loadTag("322374539", "160x600", "322374539");
-                            })
-                        }
-                        catch (error) {}
-                        `
-                    }}
-                />
-            </div>
+            <script type="text/javascript">
+                try {
+                    window._mNHandle.queue.push(function (){
+                        window._mNDetails.loadTag("322374539", "160x600", "322374539");
+                    })
+                }
+                catch (error) {}
+            </script>
+        </div>
         </React.Fragment>
     ]);
 
