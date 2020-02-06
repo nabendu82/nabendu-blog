3b5d0ac8b119a6e8795247931d70607612fbbab5
nabendu82 <nabendu.biswas@gmail.com>
2020-02-06 19:45:01 +0530

    media.net ads again

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,13 +10,36 @@ const safePrefix = require("./src/utils/safePrefix").default;
 exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
 
     setHeadComponents([
-
+        <script
+        dangerouslySetInnerHTML={{
+            __html:`
+            window._mNHandle = window._mNHandle || {};
+            window._mNHandle.queue = window._mNHandle.queue || [];
+            medianet_versionId = "3121199";
+            `
+        }}
+    />,
+    <script src="//contextual.media.net/dmedianet.js?cid=8CUVUWCU4" async="async" />
     ]);
 
     setPostBodyComponents([
         <React.Fragment>
             <script src={safePrefix('assets/js/plugins.js')}/>
             <script src={safePrefix('assets/js/main.js')}/>
+            <div id="322374539">
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html:`
+                        try {
+                            window._mNHandle.queue.push(function (){
+                                window._mNDetails.loadTag("322374539", "160x600", "322374539");
+                            })
+                        }
+                        catch (error) {}
+                        `
+                    }}
+                />
+            </div>
         </React.Fragment>
     ]);
 
